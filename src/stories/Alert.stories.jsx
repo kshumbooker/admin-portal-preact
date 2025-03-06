0beadec7a81f13a4ba1bb17f3e0340e8c8e95be6
Getronics-PTomar <prashant.tomar@getronics.com>
2025-03-06 09:20:15 +0000

    Changed the components as per need in the booker project

--- src/stories/Alert.stories.jsx
+++ src/stories/Alert.stories.jsx
@@ -7,8 +7,8 @@ export default {
   tags: ['autodocs']
 }
 
-export const ValidationError = () => <Alert type="danger" msg="Invalid User ID / Password combination, please try again" />
+export const ValidationError = () => <Alert type="danger" msg="Invalid User ID / Password combination, please try again" classes="tw:flex tw:items-center tw:p-4 tw:my-4 tw:text-sm tw:rounded-lg tw:max-w-lg tw:m-auto" />
 
-export const VerficationSuccess = () => <Alert type="success" msg="New login code has been sent via SMS" />
+export const VerficationSuccess = () => <Alert type="success" msg="New login code has been sent via SMS" classes="tw:flex tw:items-center tw:p-4 tw:my-4 tw:text-sm tw:rounded-lg tw:max-w-lg tw:m-auto" />
 
-export const VerificationInfo = () => <Alert type="info" msg="You will be able to request another code in 30 seconds" />
+export const VerificationInfo = () => <Alert type="info" msg="You will be able to request another code in 30 seconds" classes="tw:flex tw:items-center tw:p-4 tw:my-4 tw:text-sm tw:rounded-lg tw:max-w-lg tw:m-auto" />
